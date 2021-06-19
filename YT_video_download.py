1a442cc25cb146f029344b2fe504475be528e607
viveklele <viveklele123@gmail.com>
2021-06-19 09:12:24 +0530

    miss no in list

--- YT_video_download.py
+++ YT_video_download.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 link = input("Enter link: ")
 video = YouTube(link)
-stream = video.streams.get_highest_resolution()
+stream = video.streams.get_by_resolution("1080p")
 print("Download inprogress...")
 stream.download()
 print("Download complete")
